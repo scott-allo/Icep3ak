35423fd5aedc82e3cf5ce772cb97ad6206c8c61e
scott <scott.allo@laplateforme.io>
2025-06-17 11:37:24 +0200

    Résolution des erreurs de routage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import GlitchText from '../components/GlitchText';
 
 const Home = ({ enableAudio }) => {
@@ -7,8 +8,8 @@ const Home = ({ enableAudio }) => {
       <GlitchText text="IC3PEAK" />
       <nav className="nav">
         <ul className="nav__list">
-          <li><a href="/music" className="nav__link" onClick={enableAudio}>Musique</a></li>
-          <li><a href="/videos" className="nav__link" onClick={enableAudio}>Vidéos</a></li>
+          <li><Link to="/music" className="nav__link" onClick={enableAudio}>Musique</Link></li>
+          <li><Link to="/videos" className="nav__link" onClick={enableAudio}>Vidéos</Link></li>
         </ul>
       </nav>
     </div>
